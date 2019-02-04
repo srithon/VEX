cca87efe7e2eb37e3200c502e90ffdc0e4f9948f
Dale Nacianceno <dale.nash01@gmail.com>
2019-02-03 20:07:16 -0500

    Changed LeftMotorBack from PORT18 to PORT8

--- robot-config.h
+++ robot-config.h
@@ -3,7 +3,7 @@ vex::controller Controller1;
 vex::motor RightMotorFront = vex::motor(vex::PORT17, true);
 vex::motor RightMotorBack = vex::motor(vex::PORT10, true);
 vex::motor LeftMotorFront = vex::motor(vex::PORT19, false);
-vex::motor LeftMotorBack = vex::motor(vex::PORT18, false);
+vex::motor LeftMotorBack = vex::motor(vex::PORT8, false);
 vex::motor RollerMotor = vex::motor(vex::PORT20, true);
 vex::motor LauncherMotor = vex::motor(vex::PORT2, true);
 vex::motor OneBarMotor = vex::motor(vex::PORT16, true);
