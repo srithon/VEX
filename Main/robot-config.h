2e87d4b6ce0832aa2749adb9503fb50c06a317f4
srithon <36751023+srithon@users.noreply.github.com>
2019-03-28 21:54:56 -0400

    fixed

--- Main/robot-config.h
+++ Main/robot-config.h
@@ -1,27 +1,17 @@
-/**************************************************
-*
-*
-* Team: 750E
-* Game: Turning Point
-* Header File
-*
-*
-***************************************************/
-
-//uses namespace for cleaner code;
 using namespace vex;
 
 /**************************************************/
 /*Motor Port Declaration Panel*/
 
-const int FRONT_RIGHT_PORT = PORT17;
-const int BACK_RIGHT_PORT = PORT10;
-const int FRONT_LEFT_PORT = PORT19;
-const int BACK_LEFT_PORT = PORT18;
+const int FRONT_RIGHT_PORT = PORT10;
+const int BACK_RIGHT_PORT = PORT19;
+const int FRONT_LEFT_PORT = PORT2;
+const int BACK_LEFT_PORT = PORT11;
 
-const int ROLLER_PORT = PORT20;
+const int ROLLER_PORT = PORT7;
+const int ROLLER2_PORT = PORT18;
 
-const int LAUNCHER_PORT = PORT2;
+const int LAUNCHER_PORT = PORT16;
 
 /***************************************************/
 
@@ -35,14 +25,15 @@ controller Controller1 = controller();
 
 competition comp = competition();
 
-motor RightMotorFront = motor(FRONT_RIGHT_PORT, true);
-motor RightMotorBack = motor(BACK_RIGHT_PORT, true);
-motor LeftMotorFront = motor(FRONT_LEFT_PORT, false);
-motor LeftMotorBack = motor(BACK_LEFT_PORT, false);
+motor rightMotorF = motor(FRONT_RIGHT_PORT, false);
+motor rightMotorB = motor(BACK_RIGHT_PORT, true);
+motor leftMotorF = motor(FRONT_LEFT_PORT, true);
+motor leftMotorB = motor(BACK_LEFT_PORT, false);
 
-motor RollerMotor = motor(ROLLER_PORT, false);
+motor rollerMotor = motor(ROLLER_PORT, true);
+motor rollerMotor2 = motor(ROLLER2_PORT, false);
 
-motor LauncherMotor = motor(LAUNCHER_PORT, true);
+motor launcherMotor = motor(LAUNCHER_PORT, true);
 /**************************************************/
 
 controller::axis* VERTICAL_AXIS = &Controller1.Axis3;
@@ -52,7 +43,7 @@ controller::button* SLOW_LEFT = &Controller1.ButtonLeft;
 controller::button* SLOW_RIGHT = &Controller1.ButtonRight;
 controller::button* SLOW_UP = &Controller1.ButtonUp;
 
-controller::button* INTAKE_IN = &Controller1.ButtonL1;
-controller::button* INTAKE_OUT = &Controller1.ButtonL2;
+controller::button* INTAKE_IN = &Controller1.ButtonL2;
+controller::button* INTAKE_OUT = &Controller1.ButtonR2;
 
 controller::button* LAUNCH_BUTTON = &Controller1.ButtonR1;
